9b203433334fff5217259dea957fbb8c63960d0a
Miraide Augusto Bassam <77417301+miraidebassam@users.noreply.github.com>
2023-02-15 15:20:28 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import Model.Personne;
 public class Main {
     public static void main(String[] args) {
 
-        Personne p = new Personne("ndongo","cheikh",'M',24);
-        Personne p1 = new Personne("Diouf","Samba",'M',24);
+        Personne p = new Personne("miraide","bassam",'F',23);
+        Personne p1 = new Personne("Diouf","Samba",'M',40);
         p.sePresenter();
         p1.sePresenter();
         System.out.println(Personne.getNbPersonnses());
